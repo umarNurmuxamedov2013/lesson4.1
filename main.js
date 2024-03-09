ff9de892a8debe421deade71dfc8825896579003
umarNurmuxamedov2013 <nurmuxamedovumar@gmail.com>
2024-03-09 10:46:08 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// alert("Hozir savollarga javob berasiz va tog'risini yozasiz")
+let Ism = prompt ("Ismingizni kiriting");
+let Familiya = prompt ("Familiyangizni kiriting");
+let Yosh = prompt ("Yoshingizni kiriting")
+let Yil = prompt ("Tug'ulgan yilingizni kiriting")
+let Kun = prompt ("Tug'ulgan kuninggizni kiriting")
+let Raqam = prompt ("Telefon raqamingizni kiriting")
+let Karta = prompt ("Kartangizni kodini emaildan nurmuxamedovumar@gmail.com ga tashlab yuboring")
+alert("Rostanham hamma savollarga tog'ri javob berdingizmi ?")
+
+console.log("Name =" + Ism);
+console.log("Surname =" + Familiya);
+console.log("Age =" + Yosh);
+console.log("Year =" + Yil);
+console.log("Day =" + Kun);
+console.log("Number =" + Raqam);
+console.log("Card number =" + Karta);
